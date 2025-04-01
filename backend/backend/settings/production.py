d54ed9872b3d1026257f06bd69c976a8613895ee
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2025-04-02 00:16:25 +0100

    Fixed host issue

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -3,7 +3,7 @@ from urllib.parse import urlparse
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["sportaai.onrender.com"]
+ALLOWED_HOSTS = ["sportaai.onrender.com",]
 
 
 tmpPostgres = urlparse(config("DATABASE_URL"))
