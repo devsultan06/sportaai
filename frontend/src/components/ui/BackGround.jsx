06a2dd1094baf822a31fb55182bcf73cd02ba51d
Sultan A. Dev <sultanabaniks@gmail.com>
2025-04-01 14:56:37 +0100

    enhance Background component: load background image asynchronously and display children only after loading

--- frontend/src/components/ui/BackGround.jsx
+++ frontend/src/components/ui/BackGround.jsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
+
 const Background = ({ children, bgImage }) => {
-    return (
-        <div
-            className="font-metropolis w-full min-h-screen bg-cover bg-center bg-no-repeat"
-            style={{ backgroundImage: `url(${bgImage})` }} 
-        >
-            {children}
-        </div>
-    );
+  const [backgroundLoaded, setBackgroundLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = bgImage;
+    img.onload = () => {
+      setBackgroundLoaded(true);
+      document.body.style.backgroundImage = `url(${bgImage})`;
+      document.body.style.backgroundSize = "cover";
+      document.body.style.backgroundPosition = "center";
+      document.body.style.backgroundRepeat = "no-repeat";
+    };
+  }, [bgImage]);
+
+  return (
+    <div className="font-metropolis w-full min-h-screen">
+      {backgroundLoaded && children}
+    </div>
+  );
 };
 
 export default Background;
