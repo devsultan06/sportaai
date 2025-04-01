bcc67c1186ecd20d020e5e93bbb18c9eaff760c7
Yusuff Abdulhakeem <hakeemyusuff19@gmail.com>
2025-04-01 22:55:48 +0100

    Separated settings into local and prod set up remote db and remote redis

---
+++ backend/backend/settings/production.py
@@ -0,0 +1,17 @@
+from .base import *
+from urllib.parse import urlparse
+
+DEBUG = False
+
+
+tmpPostgres = urlparse(config("DATABASE_URL"))
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": tmpPostgres.path.replace("/", ""),
+        "USER": tmpPostgres.username,
+        "PASSWORD": tmpPostgres.password,
+        "HOST": tmpPostgres.hostname,
+        "PORT": 5432,
+    }
+}
